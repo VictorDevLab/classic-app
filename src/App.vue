4c55bb9bb09066e01bd6b82eade3dffd549a65c3
Victor Koome <koomev786@gmail.com>
2024-05-04 23:58:12 +0300

    create classes for msgs conditionally

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <p
+    v-bind:class="{
+      bold: true,
+      italic: true,
+      valid: isValid,
+      invalid: !isValid,
+    }"
+  >
+    Lorem ipsum dolor
+  </p>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      isValid: false,
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.bold {
+  font-weight: bolder;
+}
+.italic {
+  font-style: italic;
+}
+.valid {
+  color: forestgreen;
+}
+.invalid {
+  color: orangered;
 }
 </style>
