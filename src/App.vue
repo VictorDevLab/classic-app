4868c6d2cf6b0049952667d19c8c8b12b1fe99c8
Victor Koome <koomev786@gmail.com>
2024-05-11 20:35:54 +0300

    add nested card loops

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,18 @@
 <template>
-  <!-- <p>{{ thisInArrowFunc() }}</p> -->
-  <p>{{ username }}</p>
-  <button @click="changeName('whasdrdt')">Change Name</button>
+  <p v-for="(name, idx) in names" :key="name.id">
+    {{ idx }} {{ name.first }} {{ name.last }}
+  </p>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      username: "Obi-Wan Kenobi",
+      names: [
+        { id: 0, first: "John", last: "Doe" },
+        { id: 1, first: "Jane", last: "Doe" },
+      ],
     };
-  },
-  methods: {
-    changeName(name) {
-      this.username = name;
-    },
   },
 };
 </script>
