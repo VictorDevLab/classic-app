d13847224506ea0d9820a74272ae01ddd83032d3
Victor Koome <koomev786@gmail.com>
2024-05-11 21:52:36 +0300

    added lodash to sort the arrays

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,34 @@
 <template>
-  <p v-for="(name, idx) in names" :key="name.id">
-    {{ idx }} {{ name.first }} {{ name.last }}
-  </p>
+  <p v-for="user in legalUsers" :key="user.name">{{ user.name }}</p>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      names: [
-        { id: 0, first: "John", last: "Doe" },
-        { id: 1, first: "Jane", last: "Doe" },
+      users: [
+        { name: "John", age: 33 },
+        { name: "Jane", age: 33 },
+        { name: "Jack", age: 17 },
+        { name: "Jill", age: 16 },
       ],
     };
   },
+  computed: {
+    legalUsers() {
+      return this.users.filter((user) => user.age >= 18);
+    },
+  },
 };
 </script>
+
+<style>
+div {
+  background-color: rgb(132, 118, 118);
+  border: 2px solid white;
+}
+
+p {
+  background-color: rgb(124, 98, 149);
+}
+</style>
